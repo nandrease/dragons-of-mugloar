fa4edd25b01b204f9dcd58241a09947cb5bdd962
nandrease <neeme.andreas.eller@gmail.com>
2020-04-06 01:11:39 +0300

    Start screen

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Welcome to Mugloar</h1>
+    <h3>Dragon services needed!</h3>
+    <p>
+      <AppButton>Start new game</AppButton>
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppButton from "./components/AppButton";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    AppButton
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100%;
 }
 </style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "Click here!"
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+button {
+  border: 3px solid;
+  padding: 1em 2em;
+  text-transform: uppercase;
+  background: transparent;
+  font-weight: 500;
+}
+button:hover {
+  background: rgba(255, 255, 255, 0.8);
+  font-weight: bold;
+}
+</style>
