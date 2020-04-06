9401c0f831f3852b29e11e677acc97ee5bd06776
nandrease <neeme.andreas.eller@gmail.com>
2020-04-06 20:13:08 +0300

    Initial dist subtree commit

---
+++ precache-manifest.ac247038099d3f9ef4af4e43f3786b63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0baa3b373a4c567b4e2",
+    "url": "/dragons-of-mugloar/css/app.d06c9bf8.css"
+  },
+  {
+    "revision": "e2b26e6fe243936bf219",
+    "url": "/dragons-of-mugloar/css/quest.8004d9de.css"
+  },
+  {
+    "revision": "a492b9bad71c3be287b8",
+    "url": "/dragons-of-mugloar/css/shop.8004d9de.css"
+  },
+  {
+    "revision": "839d4e425bac55b28292d097b94639cb",
+    "url": "/dragons-of-mugloar/img/vintage-bg.839d4e42.jpg"
+  },
+  {
+    "revision": "ccdc3ba33773e9496a67b3046ee87650",
+    "url": "/dragons-of-mugloar/index.html"
+  },
+  {
+    "revision": "e0baa3b373a4c567b4e2",
+    "url": "/dragons-of-mugloar/js/app.bff87ea7.js"
+  },
+  {
+    "revision": "bfe01cdff6143480e990",
+    "url": "/dragons-of-mugloar/js/chunk-vendors.d1ad901b.js"
+  },
+  {
+    "revision": "e2b26e6fe243936bf219",
+    "url": "/dragons-of-mugloar/js/quest.be865cc3.js"
+  },
+  {
+    "revision": "a492b9bad71c3be287b8",
+    "url": "/dragons-of-mugloar/js/shop.80df6dff.js"
+  },
+  {
+    "revision": "d182d3c97aaefae3471e1b8a05f27c97",
+    "url": "/dragons-of-mugloar/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dragons-of-mugloar/robots.txt"
+  }
+]);
